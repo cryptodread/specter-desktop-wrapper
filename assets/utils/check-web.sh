4b2b46c07c6d9e0638d85c6dc331f1438a9084ef
Dread <dread@start9labs.com>
2022-05-15 03:04:56 -0400

    Web UI working

--- assets/utils/check-web.sh
+++ assets/utils/check-web.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 DURATION=$(</dev/stdin)
-if (($DURATION <= 5000)); then
+if (($DURATION <= 10000)); then
     exit 60
 else
-    if ! curl --silent --fail lightning-terminal.embassy:8443 &>/dev/null; then
+    if curl --silent --fail lightning-terminal.embassy:8443 &>/dev/null; then
         echo "Lightning Terminal UI is unreachable" >&2
         exit 1
     fi
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -5,7 +5,6 @@ _term() {
   kill -TERM "$lightning_terminal_process" 2>/dev/null
 }
 
-echo -e "Starting Entrypoint..."
 echo "Configuring LiT..."
 echo "
 remote.lnd.rpcserver=lnd.embassy:10009
@@ -13,12 +12,7 @@ remote.lnd.macaroonpath=/mnt/lnd/admin.macaroon
 remote.lnd.tlscertpath=/mnt/lnd/tls.cert
 " >> /root/.lit/lit.conf
 echo "Starting LiT..."
-/bin/litd --uipassword=testing1234 --lit-dir=~/.lit --letsencrypt --letsencrypthost=lightning-terminal.embassy
-
-while true;
-do
-sleep 10; 
-done 
+/bin/litd --uipassword=testing1234 --lit-dir=~/.lit --insecure-httplisten=lightning-terminal.embassy:8443
 
 trap _term SIGTERM
 wait -n $lightning_terminal_process
